ea52e8f7119bebff7135a9a9ebc13dc30fd6b1cb
Shivrag <shivragshukla001@gmail.com>
2024-02-06 16:30:52 +0530

    Added: mouse event on star rating

--- src/component/star-rating/index.jsx
+++ src/component/star-rating/index.jsx
@@ -6,7 +6,14 @@ export default function StarRating({noOfRatings = 5}) {
 
 	const [starRatingArray, setStarRatingArray] = useState([...Array(noOfRatings).keys()])
 	const [rating, setRating] = useState(0);
+	const [hover, setHover] = useState(0);
 
+	function handleMouseHover(currentHover) {
+		setHover(currentHover)
+	}
+	function handleMouseLeave() {
+		setHover(rating)
+	}
 	function handleRating(currentRating) {
 		setRating(currentRating)
 	}
@@ -16,14 +23,19 @@ export default function StarRating({noOfRatings = 5}) {
 			<h1 style={{ color: "#FFF" }}>Star Rating</h1>
 			<div className="star-rating">
 				{
-					starRatingArray.map(index => 
-						<span key={index}>
-							<FaStar
-								onClick={() => handleRating(index + 1)}
-								className={ rating >= index+1 ? 'active' : '' }
-							/>
-						</span>
-					)
+					starRatingArray.map(index => {
+						index += 1;  
+						return (
+							<span key={index}>
+								<FaStar
+									onClick={() => handleRating(index)}
+									onMouseEnter={() => handleMouseHover(index)}
+									onMouseLeave={() => handleMouseLeave()}
+									className={ index <= ( hover || rating ) ? 'active' : '' }
+								/>
+							</span>
+						)
+					})
 				}
 				
 			</div>
